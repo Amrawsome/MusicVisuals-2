806c5fe81902cbd9437e76a8adb8a8468c537ea2
Rada1242 <d19125768@mytudublin.ie>
2022-05-02 00:35:50 +0100

    heheh

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,8 +1,5 @@
 package ie.tudublin;
-import D21125383.Test;
-import example.CubeVisual;
 import example.MyVisual;
-import example.RotatingAudioBands;
 
 public class Main
 {	
@@ -16,18 +13,12 @@ public class Main
 	public static void assignment()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new D21125383.assignment());		
-	}
-
-	public static void Assignment()
-	{
-		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new Assignment());		
 	}
+
 	public static void main(String[] args)
 	{
-		
-		Assignment();			
+		assignment();			
 	}
 
 	
