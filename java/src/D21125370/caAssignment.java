62a818fe7e41e36365e3d4311d0493c60d49e339
lxlxandrew <d21125370@mytudublin.ie>
2022-05-02 02:30:45 +0100

    Done :)

--- java/src/D21125370/caAssignment.java
+++ java/src/D21125370/caAssignment.java
@@ -20,11 +20,6 @@ public class caAssignment extends PApplet
     final int SX = 1024;
     final int SY = 600;
 
-    // for text
-    String message = "Goodbye!";
-    float x, y; // X and Y coordinates of text
-    float hr, vr;  // horizontal and vertical radius of the text
-
     
     public void keyPressed() {
 		if (key >= '0' && key <= '9') {
@@ -52,11 +47,6 @@ public class caAssignment extends PApplet
         minim = new Minim(this);
         heaven = minim.loadFile("differentheaven.mp3", 1024);
         heaven.play();
-
-        // Create the font
-        textSize(36);      
-        x = width / 2;
-        y = height / 2;
         
     }
 
@@ -65,46 +55,12 @@ public class caAssignment extends PApplet
         switch (mode) {
 			case 0:
                 {
-                    text("Goodbye!", x, y);
-
-
-
-                    // little person
-
-                    background(0);
-                    ellipseMode(CENTER);
-                    rectMode(CENTER); 
-
-                    //text
-                    text("Goodbye!", 0, 0);
-
-                    // Body
-                    stroke(0);
-                    fill(150);
-                    rect(500, 45, 20, 100);
-
-                    // Head
-                    fill(255);
-                    ellipse(500, 15, 60, 60); 
-
-                    // Eyes
-                    fill(0); 
-                    ellipse(221, 115, 16, 32); 
-                    ellipse(259, 115, 16, 32);
-
-                    // Legs
-                    stroke(0);
-                    line(230, 195, 220, 205);
-                    line(250, 195, 260, 205);  
-
-
-                    /*
                     //progress bar
                     background(0);
                     per = (float) ((per + 0.15) % 100); 
                     textSize(30);
                     text("Loading ... " + per + " %", SX / 4, (float) (SY / 2.5));
-                    rect(SX / 4, SY / 2, per * 2, 20, 7); */
+                    rect(SX / 4, SY / 2, per * 2, 20, 7); 
                     
                 }
                 break;
@@ -132,13 +88,13 @@ public class caAssignment extends PApplet
                         float x3 = sin(radians(i))*(500/angle); 
                         float y3 = cos(radians(i))*(500/angle);
                         
-                        fill (128,0,128); // purple
+                        fill(128,0,128); // purple
                         ellipse(x, y, heaven.left.get(i)*10, heaven.left.get(i)*10);
                         
-                        fill (255,255,255); // white
+                        fill(255,255,255); // white
                         rect(x3, y3, heaven.left.get(i)*20, heaven.left.get(i)*10);
                         
-                        fill (186,85,211); // medium orchid
+                        fill(186,85,211); // medium orchid
                         rect(x, y, heaven.right.get(i)*10, heaven.left.get(i)*10);
                         
                         fill(255,255,255); // white
@@ -155,5 +111,41 @@ public class caAssignment extends PApplet
 }// end class
 
 /*
+fill(255,255,255);
+textSize(64);
+text("Goodbye!", 512, -500);
+
+// little person
+
+background(0);
+ellipseMode(CENTER);
+rectMode(CENTER); 
+
+//text
+text("Goodbye!", 0, 0);
+
+float value = 50;
+float m = map(value, 0, 100, 0, width);
+ellipse(m, 200, 10, 10);
+
+// Body
+stroke(0);
+fill(128,0,128); // purple
+rect(500, 45, 20, 100);
+
+// Head
+fill(255); // white
+ellipse(m, 200, 10, 10);
+
+// Eyes
+fill(0);  // black
+ellipse(221, 115, 16, 32); 
+ellipse(259, 115, 16, 32);
+
+// Legs
+stroke(0);
+fill(255);
+line(230, 195, 220, 205);
+line(250, 195, 260, 205);  
 
 */
